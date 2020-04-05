079ba7a6f58baa72e1a83100ea98a59682c4ac87
Muhammad Farag <muhammad.farag@shopify.com>
2020-04-05 08:45:35 -0400

    Render Currently Reading shelve from list of books

--- src/App.js
+++ src/App.js
@@ -31,30 +31,30 @@ class BookShelf extends React.Component {
 
 
   render() {
-    const book1 = {
-      title: 'To Kill a Mockingbird',
-      authors: 'Harper Lee',
-      backgroundImage: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-    };
-
-    const book2 = {
-      title: "Ender's Game",
-      authors: 'Orson Scott Card',
-      backgroundImage: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-    };
+    const books = [
+      {
+        id: "1",
+        title: 'To Kill a Mockingbird',
+        authors: 'Harper Lee',
+        backgroundImage: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+      },
+      {
+        id: "2",
+        title: "Ender's Game",
+        authors: 'Orson Scott Card',
+        backgroundImage: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+      }];
 
     return <div className="bookshelf">
       <h2 className="bookshelf-title">{this.props.title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <li>
-            <Book title={book1.title} authors={book1.authors}
-                  backgroundImage={`url(${book1.backgroundImage})`}/>
-          </li>
-          <li>
-            <Book title={book2.title} authors={book2.authors}
-                  backgroundImage={`url(${book1.backgroundImage})`}/>
-          </li>
+          {books.map((book) => (
+            <li key={book.id}>
+              <Book title={book.title} authors={book.authors}
+                    backgroundImage={`url(${book.backgroundImage})`}/>
+            </li>
+          ))}
         </ol>
       </div>
     </div>;
