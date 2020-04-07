816596d7b8b60f4596c5107f441cfc354d0d6946
Muhammad Farag <muhammad.farag@shopify.com>
2020-04-07 18:49:27 -0400

    Extract Search to a separate component and use Router instead of state to manage page visibility

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 import './App.css'
 import {getAll, update} from "./BooksAPI";
 import BookShelf from "./components/BookShelf";
-import {Link} from "react-router-dom";
+import {Link, Route} from "react-router-dom";
+import Search from "./components/Search";
 
 class BooksApp extends React.Component {
   /**
@@ -22,7 +23,6 @@ class BooksApp extends React.Component {
     currentlyReading: [],
     wantToRead: [],
     read: [],
-    showSearchPage: false
   };
 
   books = [];
@@ -69,28 +69,10 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"/>
-            </div>
-          </div>
-        ) : (
+        <Route path='/search' render={() => (
+          <Search/>
+        )}/>
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -109,7 +91,8 @@ class BooksApp extends React.Component {
               <Link to={{pathname: '/search'}}>Add a book</Link>
             </div>
           </div>
-        )}
+        )}/>
+
       </div>
     )
   }
