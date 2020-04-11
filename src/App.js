859029b4dd0be7b8c71fcd5ed8471139599e4a6a
Muhammad Farag <muhammad.farag@shopify.com>
2020-04-11 15:47:06 -0400

    Move updating home state up to the app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,44 @@ import './App.css'
 import {Route} from "react-router-dom";
 import Search from "./components/Search";
 import Home from "./components/Home";
+import {getAll, update} from "./BooksAPI";
 
 class BooksApp extends React.Component {
+
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    getAll().then((r) => {
+      this.setState({
+        books: r.map(this.backendBookFormatAdapter)
+      })
+    });
+  }
+
+  backendBookFormatAdapter(v) {
+    return {
+      id: v.id,
+      title: v.title,
+      authors: v.authors,
+      backgroundImage: v.imageLinks.smallThumbnail,
+      shelf: v.shelf
+    };
+  }
+
+  moveBookToShelf = (book_id, shelf) => {
+    update({id: book_id}, shelf).then((_) => {
+      this.setState((previousState) => ({
+        books: previousState.books.map((book) => {
+          if (book.id === book_id) {
+            book.shelf = shelf
+          }
+          return book
+        })
+      }))
+    })
+  };
 
   render() {
     return (
@@ -13,7 +49,7 @@ class BooksApp extends React.Component {
           <Search/>
         )}/>
         <Route exact path='/' render={() => (
-          <Home/>
+          <Home books={this.state.books} onBookShelfChange={this.moveBookToShelf}/>
         )}/>
       </div>
     )
